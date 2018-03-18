bd423c329c16064060890ecdc80b0b48a86ed705
Alexey Orlov <orlov@bos-ltd.ru>
2018-03-19 01:35:16 +0300

    first commit

---
+++ ftabgen.py
@@ -0,0 +1,288 @@
+from numpy import pi, sin
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button, RadioButtons, AxesWidget
+
+# \/ Vertical slider \/
+
+class VertSlider(AxesWidget):
+    """
+    A slider representing a floating point range.
+
+    For the slider to remain responsive you must maintain a
+    reference to it.
+
+    The following attributes are defined
+      *ax*        : the slider :class:`matplotlib.axes.Axes` instance
+
+      *val*       : the current slider value
+
+      *hline*     : a :class:`matplotlib.lines.Line2D` instance
+                     representing the initial value of the slider
+
+      *poly*      : A :class:`matplotlib.patches.Polygon` instance
+                     which is the slider knob
+
+      *valfmt*    : the format string for formatting the slider text
+
+      *label*     : a :class:`matplotlib.text.Text` instance
+                     for the slider label
+
+      *closedmin* : whether the slider is closed on the minimum
+
+      *closedmax* : whether the slider is closed on the maximum
+
+      *slidermin* : another slider - if not *None*, this slider must be
+                     greater than *slidermin*
+
+      *slidermax* : another slider - if not *None*, this slider must be
+                     less than *slidermax*
+
+      *dragging*  : allow for mouse dragging on slider
+
+    Call :meth:`on_changed` to connect to the slider event
+    """
+    def __init__(self, ax, label, valmin, valmax, valinit=0.5, valfmt='%1.2f',
+                 closedmin=True, closedmax=True, slidermin=None,
+                 slidermax=None, dragging=True, **kwargs):
+        """
+        Create a slider from *valmin* to *valmax* in axes *ax*.
+
+        Additional kwargs are passed on to ``self.poly`` which is the
+        :class:`matplotlib.patches.Rectangle` which draws the slider
+        knob.  See the :class:`matplotlib.patches.Rectangle` documentation
+        valid property names (e.g., *facecolor*, *edgecolor*, *alpha*, ...).
+
+        Parameters
+        ----------
+        ax : Axes
+            The Axes to put the slider in
+
+        label : str
+            Slider label
+
+        valmin : float
+            The minimum value of the slider
+
+        valmax : float
+            The maximum value of the slider
+
+        valinit : float
+            The slider initial position
+
+        label : str
+            The slider label
+
+        valfmt : str
+            Used to format the slider value, fprint format string
+
+        closedmin : bool
+            Indicate whether the slider interval is closed on the bottom
+
+        closedmax : bool
+            Indicate whether the slider interval is closed on the top
+
+        slidermin : Slider or None
+            Do not allow the current slider to have a value less than
+            `slidermin`
+
+        slidermax : Slider or None
+            Do not allow the current slider to have a value greater than
+            `slidermax`
+
+
+        dragging : bool
+            if the slider can be dragged by the mouse
+
+        """
+        AxesWidget.__init__(self, ax)
+
+        self.valmin = valmin
+        self.valmax = valmax
+        self.val = valinit
+        self.valinit = valinit
+        self.poly = ax.axhspan(valmin, valinit, 0, 1, **kwargs)
+
+        self.hline = ax.axhline(valinit, 0, 1, color='r', lw=1)
+
+        self.valfmt = valfmt
+        ax.set_xticks([])
+        ax.set_ylim((valmin, valmax))
+        ax.set_yticks([])
+        ax.set_navigate(False)
+
+        self.connect_event('button_press_event', self._update)
+        self.connect_event('button_release_event', self._update)
+        if dragging:
+            self.connect_event('motion_notify_event', self._update)
+        self.label = ax.text(0.5, 1.03, label, transform=ax.transAxes,
+                             verticalalignment='center',
+                             horizontalalignment='center')
+
+        self.valtext = ax.text(0.5, -0.03, valfmt % valinit,
+                               transform=ax.transAxes,
+                               verticalalignment='center',
+                               horizontalalignment='center')
+
+        self.cnt = 0
+        self.observers = {}
+
+        self.closedmin = closedmin
+        self.closedmax = closedmax
+        self.slidermin = slidermin
+        self.slidermax = slidermax
+        self.drag_active = False
+
+    def _update(self, event):
+        """update the slider position"""
+        if self.ignore(event):
+            return
+
+        if event.button != 1:
+            return
+
+        if event.name == 'button_press_event' and event.inaxes == self.ax:
+            self.drag_active = True
+            event.canvas.grab_mouse(self.ax)
+
+        if not self.drag_active:
+            return
+
+        elif ((event.name == 'button_release_event') or
+              (event.name == 'button_press_event' and
+               event.inaxes != self.ax)):
+            self.drag_active = False
+            event.canvas.release_mouse(self.ax)
+            return
+
+        val = event.ydata
+        if val <= self.valmin:
+            if not self.closedmin:
+                return
+            val = self.valmin
+        elif val >= self.valmax:
+            if not self.closedmax:
+                return
+            val = self.valmax
+
+        if self.slidermin is not None and val <= self.slidermin.val:
+            if not self.closedmin:
+                return
+            val = self.slidermin.val
+
+        if self.slidermax is not None and val >= self.slidermax.val:
+            if not self.closedmax:
+                return
+            val = self.slidermax.val
+
+        self.set_val(val)
+
+    def set_val(self, val):
+        xy = self.poly.xy
+        xy[1] = 0, val
+        xy[2] = 1, val
+        self.poly.xy = xy
+        self.valtext.set_text(self.valfmt % val)
+        if self.drawon:
+            self.ax.figure.canvas.draw_idle()
+        self.val = val
+        if not self.eventson:
+            return
+        for cid, func in self.observers.items():
+            func(val)
+
+    def on_changed(self, func):
+        """
+        When the slider value is changed, call *func* with the new
+        slider position
+
+        A connection id is returned which can be used to disconnect
+        """
+        cid = self.cnt
+        self.observers[cid] = func
+        self.cnt += 1
+        return cid
+
+    def disconnect(self, cid):
+        """remove the observer with connection id *cid*"""
+        try:
+            del self.observers[cid]
+        except KeyError:
+            pass
+
+    def reset(self):
+        """reset the slider to the initial value if needed"""
+        if (self.val != self.valinit):
+            self.set_val(self.valinit)
+            
+# /\ Vertical slider /\
+
+def signal(amp, freq):
+    return amp * sin(2 * pi * freq * t)
+
+axis_color = 'lightgoldenrodyellow'
+
+fig = plt.figure(figsize=(12,12))
+ax = fig.add_subplot(111)
+
+# Adjust the subplots region to leave some space for the sliders and buttons
+fig.subplots_adjust(left=0.25, bottom=0.50)
+
+t = np.arange(0.0, 1.0, 0.001)
+amp_0 = 5
+freq_0 = 3
+
+# Draw the initial plot
+# The 'line' variable is used for modifying the line later
+[line] = ax.plot(t, signal(amp_0, freq_0), linewidth=2, color='red')
+ax.set_xlim([0, 1])
+ax.set_ylim([-10, 10])
+
+# Add two sliders for tweaking the parameters
+
+# ~ # Define an axes area and draw a slider in it
+# ~ amp_slider_ax  = fig.add_axes([0.25, 0.15, 0.65, 0.03], facecolor=axis_color)
+# ~ amp_slider = VertSlider(amp_slider_ax, 'Amp', 0.1, 10.0, valinit=amp_0)
+
+# ~ # Draw another slider
+# ~ freq_slider_ax = fig.add_axes([0.25, 0.1, 0.65, 0.03], facecolor=axis_color)
+# ~ freq_slider = VertSlider(freq_slider_ax, 'Freq', 0.1, 30.0, valinit=freq_0)
+
+sl_n, sl_step, sl_x, sl_y, sl_w, sl_h = (2, 0.05, 0.25, 0.03, 0.03, 0.4)
+sl = [None] * sl_n
+
+# Define an axes area and draw a slider in it
+slider_ax = fig.add_axes([sl_x, sl_y, sl_w, sl_h], facecolor=axis_color)
+sl[0] = VertSlider(slider_ax, 'Freq', 0.1, 30.0, valinit=freq_0)
+
+# Draw another slider
+slider_ax  = fig.add_axes([sl_x + sl_step, sl_y, sl_w, sl_h], facecolor=axis_color)
+sl[1] = VertSlider(slider_ax, 'Amp', 0.1, 10.0, valinit=amp_0)
+
+# Define an action for modifying the line when any slider's value changes
+def sliders_on_changed(val):
+    line.set_ydata(signal(sl[1].val, sl[0].val))
+    fig.canvas.draw_idle()
+
+for slider in sl:
+    slider.on_changed(sliders_on_changed)
+
+# Add a button for resetting the parameters
+# ~ reset_button_ax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
+reset_button_ax = fig.add_axes([0.025, 0.5, 0.15, 0.04])
+reset_button = Button(reset_button_ax, 'Reset', color=axis_color, hovercolor='0.975')
+def reset_button_on_clicked(mouse_event):
+    for slider in sl:
+        slider.reset()
+
+reset_button.on_clicked(reset_button_on_clicked)
+
+# Add a set of radio buttons for changing color
+color_radios_ax = fig.add_axes([0.025, 0.6, 0.15, 0.15], facecolor=axis_color)
+color_radios = RadioButtons(color_radios_ax, ('red', 'blue', 'green'), active=0)
+def color_radios_on_clicked(label):
+    line.set_color(label)
+    fig.canvas.draw_idle()
+color_radios.on_clicked(color_radios_on_clicked)
+
+plt.show()
